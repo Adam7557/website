fec91fb4a1a6c18e672ebb2888ab38e088f2106c
Shannon Peng <shannonxpassion@gmail.com>
2015-04-22 22:35:50 -0400

    Fix .panel in CSS, fetch CSS with blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,31 +2,31 @@
 
 @section('content')
     <div class="jumbotron">
-        <img id="icon" src="images/logo.png" />   <!--We'll turn this jumbotron into a slider later-->
+        <img id="slider-logo" src="images/logo.png" />   <!--We'll turn this jumbotron into a slider later-->
     </div>
     
     <div class="row">
         <a href="#apply">
-            <div class="col-md-3 panel" id="apply">
+            <div class="col-md-3 section" id="apply">
               <h3><center>apply</center></h3>     
             </div>
         </a>
         
         <a href="{{route('about')}}">
-            <div class="col-md-3 panel" id="about">
+            <div class="col-md-3 section" id="about">
                 <!--<img src="images/favicon.png">-->
                 <h3><center>about</center></h3>
             </div>
         </a>
         
         <a href="{{route('contact')}}">       
-            <div class="col-md-3 panel" id="contact">
+            <div class="col-md-3 section" id="contact">
                 <h3><center>contact</center></h3>
             </div>
         </a>
         
         <a href="#status">    
-            <div class="col-md-3 panel" id="status">
+            <div class="col-md-3 section" id="status">
                 <h3><center>loan status</center></h3>
             </div>
         </a>
